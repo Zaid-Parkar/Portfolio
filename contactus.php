4a2cc345aaaa72eae0e660dd706364a876fbb93b
Zaid Parkar <zaids.parkar@gmail.com>
2022-11-26 02:00:47 +0530

    portfolio update 2

--- contactus.php
+++ contactus.php
@@ -2,11 +2,12 @@
     
     $Name = $_POST['Name'];
 	$Email = $_POST['Email'];
+	$Subject = $_POST['Subject'];
 	$Message = $_POST['Message'];
-
+	
 	// Database connection
 	$conn = mysqli_connect('localhost','root','','test');
-	$name= "INSERT into contactus(`Name`,`Email`,`Message`) values('$Name','$Email','$Message')";
+	$name= "INSERT into test(`Name`,`Email`,`Subject`,`Message`) values('$Name','$Email','$Subject','$Message')";
     $execute = mysqli_query($conn,$name);
     
 	if($execute){
